ec61ecac3ca059209a201c71b1ac704d38156358
Shahnawaz khan <79746456+shahnawazkhan7991@users.noreply.github.com>
2021-08-27 15:51:42 +0530

    Add files via upload

---
+++ Datatypes-class3.js
@@ -0,0 +1,42 @@
+//primitive data types
+//string
+let name = 'shahnawaz';
+console.log("my string is " + name);
+console.log("my data is "+(typeof name));
+
+//Numbers
+let marks = 34.4;
+console.log("Data type is "+ (typeof marks));
+
+//Boolean
+let isDriver = true;
+console.log("my data type is "+(typeof isDriver));
+
+//Null
+let nullvar = null;
+console.log("my data type is "+(typeof nullvar));
+
+//undefined
+let undef = undefined;
+console.log("my data tupe is "+(typeof undef));
+
+//Refrence Data type-
+
+//Array
+let arr1 =[1, 2, 3, 4, false, "string"];
+console.log("my data type is "+(typeof arr1));
+
+//object literals
+let stMarks = {
+    shahnawaz:94,
+    Rohan:34,
+    shahan:95
+}
+console.log(typeof stMarks);
+function findName(){
+
+}
+console.log(typeof findName);
+let date = new Date();
+console.log(typeof date);
+// console.log(date);
