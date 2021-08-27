880573d74d8dc344b97be98cd40aa3271a8ddef5
Shahnawaz khan <79746456+shahnawazkhan7991@users.noreply.github.com>
2021-08-27 09:32:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.time("mycode")
+console.log("Hello js");
+console.assert(589>189,"age>189 is not possible");
+console.table({shahnawaz:'this', age:22});
+// console.error("This is an error");
+console.timeEnd("Your code took");
+for(i=0; i<4; i++){
+    console.count(i);
+}
+console.warn("This is a warning");
